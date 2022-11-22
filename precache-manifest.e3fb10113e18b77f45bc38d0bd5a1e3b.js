9e3dde6f72a68a3ee31a9031882c3d4ef8b14bda
gaykawadsubodh70 <gaykawadsubodh70@gmail.com>
2022-11-23 00:16:00 +0530

    Updates

---
+++ precache-manifest.e3fb10113e18b77f45bc38d0bd5a1e3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9539def85ce7abce61ed",
+    "url": "/static/css/main.1f0ce04c.chunk.css"
+  },
+  {
+    "revision": "9539def85ce7abce61ed",
+    "url": "/static/js/main.9539def8.chunk.js"
+  },
+  {
+    "revision": "5b8e6af12871e39823d8",
+    "url": "/static/css/1.2abdb2e0.chunk.css"
+  },
+  {
+    "revision": "5b8e6af12871e39823d8",
+    "url": "/static/js/1.5b8e6af1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "885327321794d77a6b6f7ab7fd42d271",
+    "url": "/static/media/cryptocurrency.88532732.png"
+  },
+  {
+    "revision": "3756f4eb8ff8a460f53f4e1df90b6435",
+    "url": "/index.html"
+  }
+];
